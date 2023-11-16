ab252680bea727043b4acb19a585d69cbd7081c1
ry4nS2 <133468374+ry4nS2@users.noreply.github.com>
2023-11-16 16:11:05 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-alert('Bryan broxa');
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = function () {
+        tocaSom('#som_tecla_pom');
+    }
+
+    contador = contador + 1;
+
+    console.log(contador);
+}
